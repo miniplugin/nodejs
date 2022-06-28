321c4a60942690ec3c25f4c625c76c233b832dbd
kimilguk <boramcom@daum.net>
2022-06-28 03:22:09 +0000

    02_03교시 작업 소스 입니다.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
+//추가작업 시작(아래)
+//===== MySQL 데이터베이스를 사용할 수 있도록 하는 mysql 모듈 불러오기 =====//
+var mysql = require('mysql');
+
+//===== MySQL 데이터베이스 연결 설정 =====//
+var pool = mysql.createPool({
+	connectionLimit: 30,
+	host: 'localhost',
+	user: 'root',
+	password: 'apmsetup',
+	database: 'nodejs',
+	debug: false,
+});
+
+pool.getConnection(function (err, conn) {
+	if (err) {
+		if (conn) {
+			conn.release(); // 반드시 해제해야 함
+		}
+		console.log(err); //이 콘솔은 서버 터미널에서 확인가능하다. 크롬 브라우저의 콘솔이 아니다.
+		return;
+	} else {
+		console.log('데이터베이스 접속에 성공하였습니다.');
+	}
+});
 module.exports = router;
