fa03730b064726b309f8a7006bcbaf89223fa664
kimilguk <boramcom@daum.net>
2022-07-19 07:34:28 +0000

    09_02교시 작업 소스

--- routes/users.js
+++ routes/users.js
@@ -115,8 +115,8 @@ router.route('/listuser').get(function(req,res){
 	var keyword = req.query.keyword || ''; //현재 검색어가 없으면 기본값''
 	var page = req.query.page || 1; //현재 페이지 없으면 기본값 1
 	var perPage = 5; //1페이지당 보여줄 개수
-	var totalPage = 0;//전체 페이지
-	var totalCnt = 0;//전회 회원 수
+	var totalPage = 0;//전체 페이지 초기값
+	var totalCnt = 0;//전회 회원 수 초기값
 	//아래 3개는 하단 페이지내비 때문에 추가
 	var perPageNavi = 10; //하단 페이지네비 개수
 	var startPage = ((page-1)/perPageNavi)*perPageNavi+1;//하단 페이지네비의 시작 페이지
@@ -133,7 +133,7 @@ router.route('/listuser').get(function(req,res){
           //var exec = conn.query("select ?? from ??",[columns,tablename],function(err,rows){ 프로시저 사용으로 생략
 			var exec = conn.query("CALL PROC_search(?,?,?,@O_cnt)",[page,perPage,keyword],function(err,rows){
                 conn.query('SELECT @O_cnt AS totalCnt',function(err,rows2){
-					console.log('사용자 리스트 페이징번호:' + rows2[0]['pageCnt']);
+					console.log('사용자 전체 개수:' + rows2[0]['totalCnt']);
 					totalCnt = rows2[0]['totalCnt'];
 					totalPage = Math.ceil(totalCnt/perPage);//Math.ceil(올림),Math.floor(내림),Math.round(반올림)
 					if(endPage > totalPage) {
