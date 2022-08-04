3b83ef2e1c70b4c06dd8ce8c6df1405b4ff372bb
kimilguk <boramcom@daum.net>
2022-08-04 14:07:06 +0000

    13_01교시 작업 소스

--- chart-app/src/App.js
+++ chart-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Chart2 from './Chart';//Chart.js 생략
 
 function App() {
@@ -32,67 +32,7 @@ function App() {
 	  var {red, blue, yellow, green, purple, orange} = jsonData;//객체의 분배할당 이라고 한다.
 	  console.log(jsonData.red, red); //객체의 red와 분배 할당된 red는 같은 값을 가진다.
   }
-  
-  useEffect( () => { //화면에 변화가 있는지 확인 후 실행할 때(=화면이 html객체모두 로딩 후) useEffect 함수를 사용한다.
-	    var url = 'https://nodejs-jvbqr.run.goorm.io/chart/getdata';
-		fetch (url, {method:'get'})
-			.then (response => response.json()) //응답데이터를 json 형태로 변환
-			.then (contents => { //json으로 변환된 응답데이터인 contents 를 가지고 구현하는 내용
-				var jsonData=contents[0]; 
-				console.log ('JSON--------------: ', jsonData);
-			
-				//var btnVote = document.getElementById('btnVote');//투표하기 버튼객체 생성
-				//var selVote = document.getElementById('selVote');//좋아하는색성 선택객체 생성
-				//var jsonData = {"red":12,"blue":19,"yellow":3,"green":5,"purple":2,"orange":3};//초기 json 데이터객체 생성
-				//var jsonData = [12, 19, 3, 5, 2, 3];//참조: 자바스크립트의 배열구조
-				var ctx = document.getElementById('myChart').getContext('2d');//막대그래프 출력영역 객체 생성
 
-				btnVote.onclick = function () {
-					alert();
-				}
-				//myChart 시작
-				var datasets_line_bar = [{ //데이터내용 배열 객체 생성
-					label: '본인이 좋아하는 색상 설문조사',
-					data: jsonData,
-					backgroundColor: [
-						'rgba(255, 99, 132, 0.2)',
-						'rgba(54, 162, 235, 0.2)',
-						'rgba(255, 206, 86, 0.2)',
-						'rgba(75, 192, 192, 0.2)',
-						'rgba(153, 102, 255, 0.2)',
-						'rgba(255, 159, 64, 0.2)'
-					],
-					borderColor: [
-						'rgba(255, 99, 132, 1)',
-						'rgba(54, 162, 235, 1)',
-						'rgba(255, 206, 86, 1)',
-						'rgba(75, 192, 192, 1)',
-						'rgba(153, 102, 255, 1)',
-						'rgba(255, 159, 64, 1)'
-					],
-					borderWidth: 1
-				}];
-				var options_line_bar = { //데이터 출력옵션 객체
-						scales: {
-							y: {
-								beginAtZero: true
-							}
-						}
-					};
-				//myChart 객체 생성(아래)  : 여기서 ctx 영역에 Chart 데이터 객체가 출력된다.
-				var myChart = new Chart(ctx, {
-					type: 'bar',//radar, doughnut, pie, polar, bubble, scatter, area 챠트종류 선택
-					data: {
-						labels: [],
-						datasets: datasets_line_bar
-					},
-					options: options_line_bar
-				});
-				//myChart.destroy();
-			})
-			.catch (() => console.log ('에러: ' + url + '에 접속할 수 없습니다.'));
-
-	}, []);//마지막 [] 배열 값은 변경 기준 상태 값으로 디자인을 재생할 때 사용한다. 지정하지 않으면, 최초 1회만 실행 된다.
   return (
     <div className="App">
 		{/*
